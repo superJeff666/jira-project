049f97c0b32dbdf9369cf3562ca394bbd1f48c5c
superJeff666 <982855239@qq.com>
2022-05-14 10:42:20 +0800

    chore: 添加devtools分布式工具

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+loadDevTools(() =>
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 );
 
 // If you want to start measuring performance in your app, pass a function
