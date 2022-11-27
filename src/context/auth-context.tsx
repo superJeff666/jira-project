8da46f81568a974733decec36c099d5113a55d72
superJeff666 <982855239@qq.com>
2022-11-27 17:21:18 +0800

    feat: 处理me接口loading和error

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,8 +1,10 @@
 import React, { ReactNode, useState } from "react";
 import * as auth from "../auth-provider";
+import { FullPageErrorFallback, FullPageLoading } from "../components/lib";
 import { User } from "../screens/project-list/search-panel";
 import { useMount } from "../utils";
 import { http } from "../utils/http";
+import { useAsync } from "../utils/use-async";
 
 interface AuthForm {
   username: string;
@@ -33,8 +35,15 @@ const AuthContext = React.createContext<
 AuthContext.displayName = "AuthContext";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
-
+  const {
+    data: user,
+    error,
+    isError,
+    isLoading,
+    isIdle,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
   const login = (form: AuthForm) =>
     auth.login(form).then((user) => setUser(user));
   const register = (form: AuthForm) =>
@@ -42,8 +51,16 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const logout = () => auth.logout().then(() => setUser(null));
 
   useMount(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser());
   });
+
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
 
   return (
     <AuthContext.Provider
