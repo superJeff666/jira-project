dacf5d0e7f28cc765c24befb11aed7bac81df9ca
superJeff666 <982855239@qq.com>
2022-11-27 19:35:56 +0800

    feat: 修改默认路由跳转

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -22,7 +22,7 @@ export const AuthenticatedApp = () => {
               path={"/projects/:projectId/*"}
               element={<ProjectScreen />}
             />
-            <Route path="" element={<Navigate to={"/projects"} />} />
+            <Route path="*" element={<Navigate to={"/projects"} />} />
           </Routes>
         </Router>
       </Main>
